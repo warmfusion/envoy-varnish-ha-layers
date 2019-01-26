153e6e9bad94ae7303ebd3e9845446a50aa39294
Toby Jackson <toby@warmfusion.co.uk>
2019-01-26 16:14:23 +0000

    This will no longer run locally no matter what you try, its swarm or nothing

--- build-containers.sh
+++ build-containers.sh
@@ -5,12 +5,13 @@ VARNISH_VERSION=5.2.1-1~stretch
 IMAGE_TAG=${CI_BUILD_TAG:-latest}
 
 echo "Building varnish container ..."
-docker build --build-arg version=${VARNISH_VERSION} -f Dockerfile.varnish -t docker.artifactory.futurenet.com/tjackson02/varnish:${VARNISH_VERSION} .
+docker build --build-arg version=${VARNISH_VERSION} --build-arg mode=edge -f Dockerfile.varnish -t docker.artifactory.futurenet.com/tjackson02/evha-edge-varnish:${IMAGE_TAG} .
+docker build --build-arg version=${VARNISH_VERSION} --build-arg mode=origin  -f Dockerfile.varnish -t docker.artifactory.futurenet.com/tjackson02/evha-edge-varnish:${IMAGE_TAG} .
 
 
 echo "Building envoy containers ..."
-docker build --build-arg mode=edge -f Dockerfile.envoy -t docker.artifactory.futurenet.com/tjackson02/evha-edge:${IMAGE_TAG} .
-docker build --build-arg mode=origin -f Dockerfile.envoy -t docker.artifactory.futurenet.com/tjackson02/evha-origin:${IMAGE_TAG} .
+docker build --build-arg mode=edge -f Dockerfile.envoy -t docker.artifactory.futurenet.com/tjackson02/evha-edge-envoy:${IMAGE_TAG} .
+docker build --build-arg mode=origin -f Dockerfile.envoy -t docker.artifactory.futurenet.com/tjackson02/evha-origin-envoy:${IMAGE_TAG} .
 
 echo "Building test-app container ..."
 cd  src/service
@@ -20,7 +21,8 @@ cd -
 
 echo "Pushing containers..."
 
-docker push docker.artifactory.futurenet.com/tjackson02/varnish:${VARNISH_VERSION}
-docker push docker.artifactory.futurenet.com/tjackson02/evha-edge:${IMAGE_TAG}
-docker push docker.artifactory.futurenet.com/tjackson02/evha-origin:${IMAGE_TAG}
+docker push docker.artifactory.futurenet.com/tjackson02/evha-edge-varnish:${IMAGE_TAG}
+docker push docker.artifactory.futurenet.com/tjackson02/evha-origin-varnish:${IMAGE_TAG}
+docker push docker.artifactory.futurenet.com/tjackson02/evha-edge-envoy:${IMAGE_TAG}
+docker push docker.artifactory.futurenet.com/tjackson02/evha-origin-envoy:${IMAGE_TAG}
 docker push docker.artifactory.futurenet.com/tjackson02/evha-test-app:${IMAGE_TAG}
