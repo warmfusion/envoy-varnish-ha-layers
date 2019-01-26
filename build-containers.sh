fee92bd700ec0a07b38a42e5c75e9c7b834262fc
Toby Jackson <toby@warmfusion.co.uk>
2019-01-26 16:38:22 +0000

    Split varnish config and core apart to reduce build cycles and use dynamic arg for labels

--- build-containers.sh
+++ build-containers.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 
-VARNISH_VERSION=5.2.1-1~stretch
+VARNISH_VERSION=5.2.1-1
 IMAGE_TAG=${CI_BUILD_TAG:-master}
 
 
-echo "Pulling copies of containers to reduce rebuild time"
+echo "Pulling copies of containers to reduce rebuild time..."
 
+docker pull docker.artifactory.futurenet.com/tjackson02/varnish:${VARNISH_VERSION} .
 docker pull docker.artifactory.futurenet.com/tjackson02/evha-edge-varnish:${IMAGE_TAG}
 docker pull docker.artifactory.futurenet.com/tjackson02/evha-origin-varnish:${IMAGE_TAG}
 docker pull docker.artifactory.futurenet.com/tjackson02/evha-edge-envoy:${IMAGE_TAG}
@@ -14,13 +15,18 @@ docker pull docker.artifactory.futurenet.com/tjackson02/evha-origin-envoy:${IMAG
 docker pull docker.artifactory.futurenet.com/tjackson02/evha-test-app:${IMAGE_TAG}
 
 
+echo "Failing on any build errors..."
+set -e
+set -x
+
 echo "Building varnish container ..."
-docker build --build-arg version=${VARNISH_VERSION} --build-arg mode=edge -f Dockerfile.varnish -t docker.artifactory.futurenet.com/tjackson02/evha-edge-varnish:${IMAGE_TAG} .
-docker build --build-arg version=${VARNISH_VERSION} --build-arg mode=origin  -f Dockerfile.varnish -t docker.artifactory.futurenet.com/tjackson02/evha-edge-varnish:${IMAGE_TAG} .
+docker build --build-arg version=${VARNISH_VERSION} -f Dockerfile.varnish-core                     -t docker.artifactory.futurenet.com/tjackson02/varnish:${VARNISH_VERSION} .
+docker build --build-arg version=${VARNISH_VERSION} --build-arg mode=edge    -f Dockerfile.varnish -t docker.artifactory.futurenet.com/tjackson02/evha-edge-varnish:${IMAGE_TAG} .
+docker build --build-arg version=${VARNISH_VERSION} --build-arg mode=origin  -f Dockerfile.varnish -t docker.artifactory.futurenet.com/tjackson02/evha-origin-varnish:${IMAGE_TAG} .
 
 
 echo "Building envoy containers ..."
-docker build --build-arg mode=edge -f Dockerfile.envoy -t docker.artifactory.futurenet.com/tjackson02/evha-edge-envoy:${IMAGE_TAG} .
+docker build --build-arg mode=edge   -f Dockerfile.envoy -t docker.artifactory.futurenet.com/tjackson02/evha-edge-envoy:${IMAGE_TAG} .
 docker build --build-arg mode=origin -f Dockerfile.envoy -t docker.artifactory.futurenet.com/tjackson02/evha-origin-envoy:${IMAGE_TAG} .
 
 echo "Building test-app container ..."
@@ -36,3 +42,7 @@ docker push docker.artifactory.futurenet.com/tjackson02/evha-origin-varnish:${IM
 docker push docker.artifactory.futurenet.com/tjackson02/evha-edge-envoy:${IMAGE_TAG}
 docker push docker.artifactory.futurenet.com/tjackson02/evha-origin-envoy:${IMAGE_TAG}
 docker push docker.artifactory.futurenet.com/tjackson02/evha-test-app:${IMAGE_TAG}
+
+
+echo "------"
+echo "Fin..."
